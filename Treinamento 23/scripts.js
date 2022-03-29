14dc1de1c90092fd11118e78ac20472d5daa07c1
Lorraine Cristina <89432049+lorraine-crst@users.noreply.github.com>
2022-03-29 13:14:38 -0300

    Update scripts.js

--- Treinamento 23/scripts.js
+++ Treinamento 23/scripts.js
@@ -1,2 +1,24 @@
-let nome = prompt('qual seu nome?');
-console.log(nome);
+'7 pm'
+
+var x = 10;
+if(x > 5){
+    let x = 1;
+} else {
+    let x = 0;
+}
+
+console.log(x)
+
+let y = 3;
+y += 2;
+console.log(y)
+
+
+function sumUpTo(number){
+    if (number === 1) {
+        return 1;
+    }
+    return number + sumUpTo(number - 1);
+}
+
+console.log(sumUpTo(2))
