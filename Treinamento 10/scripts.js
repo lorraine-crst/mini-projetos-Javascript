d10904faba141f36d10efc171bf0d747376fa1fc
Lorraine Cristina <89432049+lorraine-crst@users.noreply.github.com>
2022-03-29 13:14:33 -0300

    Update scripts.js

--- Treinamento 10/scripts.js
+++ Treinamento 10/scripts.js
@@ -10,3 +10,12 @@ console.log(21 == 21);
 console.log("21" == 21);
 console.log(21 == 22);
 console.log(21 == "22");
+
+let hotelPrices = [300, 95, 130]
+function findDeals(price){
+    if (price < 100){
+        console.log(price)
+    }
+}
+
+hotelPrices.forEach(findDeals);
